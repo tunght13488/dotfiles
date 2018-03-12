302a1fabf2611c436190ba0d55121b786e38d860
Ha The Tung <tunght13488@gmail.com>
2018-03-12 09:32:23 +0800

    ignore tags

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git clone https://github.com/tunght13488/dotfiles.git $HOME/dotfiles
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/lukechilds/zsh-nvm.git $HOME/.zsh-nvm
+git clone https://github.com/lukechilds/zsh-better-npm-completion.git $HOME/.zsh-better-npm-completion
+git clone https://github.com/supercrabtree/k.git $HOME/k
+cd $HOME/dotfiles
+git submodule init
+git submodule update
+stow git
+stow vim
+stow tmux
+stow other
+vim +PluginInstall +qall
